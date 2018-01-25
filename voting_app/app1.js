8e21bef61999596a6ca201d3970268ad35227b99
Gayatri Arya <arya92gayatri@gmail.com>
2018-01-25 18:33:37 +0530

    upvotes working

--- voting_app/app1.js
+++ voting_app/app1.js
@@ -1,4 +1,12 @@
 class Product extends React.Component{
+	constructor(props){
+		super(props);
+		this.handleUpVote = this.handleUpVote.bind(this);
+	}
+	handleUpVote(){
+		this.props.onVote(this.props.id);
+	}
+
 	render(){
 		return(
 			<div className='item'>
@@ -7,7 +15,7 @@ class Product extends React.Component{
 			</div>
 				<div className='middle aligned content'>
 				  <div className='header'>
-				  	<a><i className='large caret up icon'/></a>{this.props.votes}
+				  	<a onClick={this.handleUpVote}><i className='large caret up icon'/></a>{this.props.votes}
 				  </div>
 					<div className='description'>
 						<a href='{this.props.url}'>{this.props.title}</a>
@@ -26,9 +34,45 @@ class Product extends React.Component{
 
 
 class ProductList extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state = {
+			products: [],
+		};
+		this.handleProductUpVote = this.handleProductUpVote.bind(this);
+	}
+
+	componentDidMount(){
+		const products = Data.sort((a, b) => {
+      	return b.votes - a.votes;
+    	});
+    	this.setState({ products: products });
+	}
+
+	handleProductUpVote(product_id){
+		const nextProducts = this.state.products.map((product) => {
+			if(product.id===product_id){
+				return Object.assign({},product, {votes: product.votes+1});
+			}
+			else{
+				return product;
+			}
+		});
+
+		this.setState({
+			products: nextProducts,
+		})
+	}
+
+	handleUpVote(product_id){
+		console.log("product id= "+product_id);
+	}
+
 	render(){
-		const productComponents = Data.map((product) => (
-				<Product key={'product-'+ product.id} id={product.id} title={product.title} description={product.description} url={product.url} votes={product.votes} submitter_avatar_url={product.submitter_avatar_url} product_image_url={product.product_image_url}/>
+		const products = this.state.products.sort((a,b) => (b.votes-a.votes));
+		const productComponents = products.map((product) => (
+				<Product key={'product-'+ product.id} id={product.id} title={product.title} description={product.description} url={product.url} votes={product.votes} submitter_avatar_url={product.submitter_avatar_url} product_image_url={product.product_image_url} onVote={this.handleProductUpVote}/>
 		 
 		));
 
